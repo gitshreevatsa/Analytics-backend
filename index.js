40965ec056ef9f8a858247b923ea2c59212c9e75
Shreyas Padmakiran <81912496+gitshreevatsa@users.noreply.github.com>
2023-10-22 17:31:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors);
 // parse application/json
 app.use(bodyParser.json());
 
